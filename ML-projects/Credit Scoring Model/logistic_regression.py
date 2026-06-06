f096225b27d60d88faa69633bbbf62dca3d30915
Rahulyadav7489 <rahulyadav748969@gmail.com>
2026-06-06 11:55:56 +0530

    Delete ML-projects directory
    changing the files name to correct ones.

--- ML-projects/Credit Scoring Model/logistic_regression.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-from split import X_train, y_train
-
-model = LogisticRegression(
-    class_weight='balanced',
-    random_state=42
-)
-
-model.fit(X_train, y_train)
